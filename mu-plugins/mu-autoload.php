366844b934a5f2979de48078350666360892ae7b
Robert Gadon <rgadon107@gmail.com>
2019-11-17 19:47:31 -0600

    Initial commit.

---
+++ mu-plugins/mu-autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( is_readable( __DIR__ . '/debug-toolkit/debug-toolkit.php' ) ) {
+	require_once __DIR__ . '/debug-toolkit/debug-toolkit.php';
+}
+
+require_once __DIR__ . '/aaa-wp-cerber.php';
+require_once __DIR__ . '/wp-migrate-db-pro-compatibility.php';
