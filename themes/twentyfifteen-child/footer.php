be1db78f4bf9a5c74ea0acc7ae71df9134b1c430
Robert Gadon <rgadon107@gmail.com>
2020-01-25 12:27:02 -0600

    Update the child theme footer template file.
    - Strip out text domain & i18n format from 'site-info' content. Not needed for this site.

--- themes/twentyfifteen-child/footer.php
+++ themes/twentyfifteen-child/footer.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * The template for displaying the footer
+ * Twentyfifteen Child theme footer template.
  *
  * Contains the closing of the "site-content" div and all content after.
  *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
+ * @package spiralWebDB\gardenOfYoga
+ * @subpackage Twentyfifteen Child
+ * @since version 1.0.0
  */
+namespace spiralWebDB\gardenOfYoga;
+
+$has_active_sidebar = is_active_sidebar( 'main-content-footer-widget' );
 ?>
-
 	</div><!-- .site-content -->
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php if ( is_active_sidebar( 'sidebar-2' )  ) : ?>
+		<?php if ( $has_active_sidebar  ) { ?>
 
- <div class="widget-area" role="complementary">
- 
-  <?php dynamic_sidebar( 'sidebar-2' ); ?>
- 
- </div>
- 
-<?php endif; ?>
-		<div class="site-info">
+        <div class="widget-area" role="complementary">
+            <?php dynamic_sidebar( 'main-content-footer-widget' ); ?>
+        </div>
+
+        <?php } ?>
+
+        <div class="site-info">
 			<?php
 				/**
 				 * Fires before the Twenty Fifteen footer text for footer customization.
@@ -36,8 +36,8 @@
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
 			}
 			?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>" class="imprint">
-				<?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?>
+			<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>" class="imprint">
+				<?php echo 'Proudly powered by WordPress'; ?>
 			</a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
