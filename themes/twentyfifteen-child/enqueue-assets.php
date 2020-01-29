f5fef175b213f4db645e0c2124aa9a6aae4dac16
Robert Gadon <rgadon107@gmail.com>
2020-01-29 15:29:30 -0600

    Add font-awesome to child theme.

--- themes/twentyfifteen-child/enqueue-assets.php
+++ themes/twentyfifteen-child/enqueue-assets.php
@@ -26,3 +26,45 @@ function enqueue_child_styles() {
 	wp_enqueue_style( $child_theme_style, get_stylesheet_directory_uri() . '/style.css', array( $parent_theme_style ), _get_asset_version( $file ) );
 }
 
+/**
+ * Font Awesome Kit Setup
+ *
+ * Load a Font Awesome kit_url to the front-end, admin back-end, and login screen area.
+ *
+ * @since 1.0.0
+ * @param false|string    $kit_url    The custom setup kit url for the registered FA account.
+ *
+ * @see https://fontawesome.com/
+ * @return void
+ */
+if (! function_exists('fa_custom_setup_kit') ) {
+	function fa_custom_setup_kit( $kit_url = '' ) {
+
+		if ( ! current_user_can( 'manage_options') ) {
+			return;
+		}
+
+		$enqueue_actions = [ 'wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts' ];
+
+		foreach ( $enqueue_actions as $action ) {
+			add_action(
+				$action,
+				function () use ( $kit_url ) {
+					wp_enqueue_script( 'font-awesome-kit', $kit_url, [], null );
+				}
+			);
+		}
+	}
+}
+
+/*
+ * Call the Font Awesome custom setup kit_url to render the FA icons.
+ *
+ * @since 1.0.0.
+ *
+ * @param string    $kit_url    The custom setup kit url for the registered FA account.
+ *
+ * @return void
+ */
+fa_custom_setup_kit('https://kit.fontawesome.com/e624e31c44.js');
+
